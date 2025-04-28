e773e4b858f33ab0a77b9c1eefae41f24df5cd24
frontrabotka <frazrabotka@gmail.com>
2025-04-28 13:25:05 +0300

    Delete update_sitemap.ps1

--- update_sitemap.ps1
+++
@@ -1,3 +0,0 @@
-# update_sitemap.ps1
-$date = Get-Date -Format "yyyy-MM-ddTHH:mm:ssZ"
-(Get-Content -path "sitemap.xml" -Raw) | ForEach-Object { $_ -replace '<lastmod>.*?</lastmod>', "<lastmod>$date</lastmod>" } | Set-Content -Path "sitemap.xml" -Encoding UTF8
